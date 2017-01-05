49628daa66f624ee13a214a579eccb3f3883ded4
Ivan Kovic <kovic.ivan@gmail.com>
2017-01-05 13:39:37 +0100

    Editor config stolen from airbnb. Babel support added

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: __dirname + "/build/assets",
-        filename: "bundle.js"
-    }
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/build/assets",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader?presets[]=es2015'
+      }
+    ]
+  }
 };
