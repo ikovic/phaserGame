994b1f8c41e8a8faa61610acf0881c4794b42888
Ivan Kovic <kovic.ivan@gmail.com>
2017-01-05 13:53:20 +0100

    Webpack configured for ES6

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,33 @@
+var path = require('path');
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: [
+    "babel-polyfill",
+    "./src/index.js"
+  ],
   output: {
     path: __dirname + "/build/assets",
+    publicPath: "assets/",
     filename: "bundle.js"
   },
   module: {
     loaders: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader?presets[]=es2015'
+        loader: "babel-loader",
+
+        // Skip any files outside of your project's `src` directory
+        include: [
+          path.resolve(__dirname, "src")
+        ],
+
+        // Only run `.js` and `.jsx` files through Babel
+        test: /\.jsx?$/,
+
+        // Options to configure babel with
+        query: {
+          plugins: ['transform-runtime'],
+          presets: ['es2015']
+        }
       }
     ]
   }
