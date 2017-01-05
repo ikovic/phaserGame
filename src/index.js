401e85ebb272b32aee48783c838dce232a2f77f5
Ivan Kovic <kovic.ivan@gmail.com>
2017-01-05 15:28:50 +0100

    Enabled phaser to work with webpack and es6

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import 'pixi';
+import 'p2';
+import 'phaser';
+
+const game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 var path = require('path');
 
+
+// Phaser webpack config
+var phaserModule = path.join(__dirname, '/node_modules/phaser/');
+var phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
+var pixi = path.join(phaserModule, 'build/custom/pixi.js');
+var p2 = path.join(phaserModule, 'build/custom/p2.js');
+
 module.exports = {
   entry: [
     "babel-polyfill",
@@ -12,6 +19,9 @@ module.exports = {
   },
   module: {
     loaders: [
+      {test: /pixi\.js/, loader: 'expose?PIXI'},
+      {test: /phaser-split\.js$/, loader: 'expose?Phaser'},
+      {test: /p2\.js/, loader: 'expose?p2'},
       {
         loader: "babel-loader",
 
@@ -30,5 +40,12 @@ module.exports = {
         }
       }
     ]
+  },
+  resolve: {
+    alias: {
+      'phaser': phaser,
+      'pixi': pixi,
+      'p2': p2
+    }
   }
 };
